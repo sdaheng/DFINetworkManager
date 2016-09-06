57c1c442c615bcaba68ecfc1eaf121ad7d3c8fc0
sdaheng <shidaheng@kibey.com>
2016-09-06 17:44:16 +0800

    modify copyright

--- DFINetworkManager/DFINetworkServiceAPIRequestDelegate.h
+++ DFINetworkManager/DFINetworkServiceAPIRequestDelegate.h
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2016 SDH
+// Copyright (c) 2014-2016 SDH
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
